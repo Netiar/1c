5fcd911bffeab87bae58307724a323270a7dcac5
Netiar <collins.odinga@student@moringaSchool.com>
2022-10-17 00:37:57 +0300

    add input and output file

---
+++ input_output.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[50];
+    int age;
+    cout<<"enter name and age:"<<endl;
+    cin>>name>>age;
+
+    // cout<<"enter age:"<<endl;
+    // cin>>age;
+
+    cout<<"The name is "<<name<<" and age is "<<age<<endl;
+    return 0;
+
+}
+
+
+
