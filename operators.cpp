3432047eea2ee1c3476e2f72b7fa0a8edb93fdc8
Netiar <collins.odinga@student@moringaSchool.com>
2022-10-26 00:47:31 +0300

    add operator file

---
+++ operators.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    //Arithmetic operators
+    // int x=10, y=3;  
+    // cout<<"addition:"<<x+y<<endl; 
+    // cout<<"subtraction:"<<x-y<<endl; 
+    // cout<<"multiplication:"<<x*y<<endl; 
+    // cout<<"division:"<<x/y<<endl; 
+    // cout<<"Modulo:"<<x%y<<endl; 
+    // cout<<"addition:"<<x+y<<endl; 
+
+
+    // post-increment and pre-increment
+    // float x=10.3222;
+    // int y; 
+    // y=++x;
+    // cout<<x<<endl<<y<<endl;
+    // y=x++;
+    // cout<<x<<endl<<y<<endl;
+
+    // Relational Operators
+    int x=3, y=6;
+    cout<<"x<y:"<<(x+2<y+1)<<endl;  
+    cout<<"x>y:"<<(x>y)<<endl;  
+    cout<<"x<=y:"<<(x<=y)<<endl;  
+    cout<<"x>=y:"<<(x>=y)<<endl;  
+    cout<<"x==y:"<<(x==y)<<endl;  
+    cout<<"x>=y:"<<(x>=y)<<endl;  
+    cout<<"x!=y:"<<(x!=y)<<endl;  
+
+
+    // logical operator
+    // int x=5, y=10;
+    // if ((x>5)&&(y<15))
+    // {
+    //     cout<<"x+y:"<<(x+y)<<endl;
+    // }
+    // else if 
+    // {
+
+    // }
+
+
+
+
+}
