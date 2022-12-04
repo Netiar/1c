359d1e2cb62ec26db1943822c49318a5ac77a9de
Netiar <collimow54@gmail.com>
2022-12-05 00:02:57 +0300

    ft: addition of bitwise operators

--- operators.cpp
+++ operators.cpp
@@ -33,19 +33,33 @@ int main ()
 
 
     // logical operator
-    int x=5, y=10;
-    if ((x>5)&&(y<15))
-    {
-        cout<<"x+y:"<<(x+y)<<endl;
-    }
-    else if((x<=5)||(y>15)) 
-    {
-        cout<<"x-y:"<<(x-y)<<endl; 
-    }
-    else
-    {
-         cout<<"!x:"<<!x<<endl;   
-    }
+    // int x=5, y=10;
+    // if ((x>5)&&(y<15))
+    // {
+    //     cout<<"x+y:"<<(x+y)<<endl;
+    // }
+    // else if((x<=5)||(y>15)) 
+    // {
+    //     cout<<"x-y:"<<(x-y)<<endl; 
+    // }
+    // else
+    // {
+    //      cout<<"!x:"<<!x<<endl;   
+    // }
+
+    //Bitwise operator
+    int x=26, y=9;
+    cout<<"x&y:"<<(x&y)<<endl; 
+    cout<<"x|y:"<<(x|y)<<endl; 
+    cout<<"x^y:"<<(x^y)<<endl;
+    cout<<"x<<1:"<<(x<<1)<<endl; 
+    cout<<"x>>1:"<<(x>>1)<<endl; 
+    cout<<"~x:"<<(~x)<<endl; 
+
+
+
+
+
 
 
 
