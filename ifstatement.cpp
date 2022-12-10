5d49e17fe5288afdcc148ec756fafad9a7824f49
Netiar <collimow54@gmail.com>
2022-12-11 01:30:36 +0300

    ft:add if statement feature code

--- ifstatement.cpp
+++ ifstatement.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 int main()
 {
+    
+  int money,age;
+  cout<<"how much money do you have:";
+  cin>>money; 
+  cout<<"enter age:"<<endl;
+  cin>>age;
+  if ((money>1200) && (age>15))
+  cout<<"ugali na Nyama kwa mama's Baze"<<endl;
+       
      
 
-     
+  cout<<"lets go home now";
 
-
-
-
-
-
+  return 0;
 
 
 
